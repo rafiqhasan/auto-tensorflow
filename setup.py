2857432a08b1847f69fd000abe6bea98f8c6fa4c
Hasan Rafiq <rafiqhasan@gmail.com>
2021-07-20 15:50:34 +0530

    TFX 1.0.0rc2 updates

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "tensorflow==2.5.0",
         "tensorflow_hub==0.12.0",
         "tensorflow_text==2.5.0",
-        "tfx==0.29.0",
+        "tfx==1.0.0rc2",
         "witwidget==1.8.0"
     ],
     classifiers=[
