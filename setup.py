1169ae8a808b42bc87c56deadd0398815db6ab30
Hasan Rafiq <rafiqhasan@gmail.com>
2021-11-18 22:39:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,7 @@ setuptools.setup(
     version="1.3.0",
     author="Hasan Rafiq",
     description="""
-    Build Low Code Automated Tensorflow, What-IF explainable models in just 3 lines of code.
-
-    To make Deep Learning on Tensorflow absolutely easy for the masses with its low code framework and also increase trust on ML models through What-IF model explainability.
+    Build Low Code Automated Tensorflow, What-IF explainable models in just 3 lines of code. To make Deep Learning on Tensorflow absolutely easy for the masses with its low code framework and also increase trust on ML models through What-IF model explainability.
     """,
     long_description=long_description,
     long_description_content_type="text/markdown",
