fec07b25e17bd3101a44c1424fea2e04c6b51c43
Hasan Rafiq <rafiqhasan@gmail.com>
2022-12-09 15:28:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="auto_tensorflow",
-    version="1.3.3",
+    version="1.3.4",
     author="Hasan Rafiq",
     description="""Build Low Code Automated Tensorflow, What-IF explainable models in just 3 lines of code. To make Deep Learning on Tensorflow absolutely easy for the masses with its low code framework and also increase trust on ML models through What-IF model explainability.""",
     long_description=long_description,
@@ -31,11 +31,11 @@ setuptools.setup(
     include_package_data=True,
     install_requires=[
         "keras-tuner==1.0.4",
-        "tensorflow==2.6.2",
-        "tensorflow_hub==0.12.0",
         "tensorflow_text==2.6.0",
         "tfx==1.4.0",
         "witwidget==1.8.0",
+        "tensorflow==2.6.2",
+        "tensorflow_hub==0.12.0",
         "tensorflow-metadata==1.4.0",
         "ipython==7.29.0",
         "tensorflow-estimator==2.6.0",
@@ -43,9 +43,19 @@ setuptools.setup(
         "tensorboard-plugin-wit==1.8.0",
         "tensorboard-data-server==0.6.1",
         "google-api-core==1.31.4",
+        "google-cloud-aiplatform==1.10.0",
         "google-cloud==0.34.0",
         "apache-beam==2.34.0",
-        "protobuf==3.19.5"
+        "protobuf==3.19.5",
+        "jupyterlab-widgets==3.0.3",
+        "PyYAML==5.4.1",
+        "pytz==2022.6",
+        "tensorflow-model-analysis==0.35.0",
+        "tensorflow-data-validation==1.4.0",
+        "tensorboard==2.6.0",
+        "six==1.15.0",
+        "requests==2.28.1",
+        "widgetsnbextension==3.6.1"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
